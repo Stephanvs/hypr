c0bcded4b6cfe36ec1405232e68642a330413d5a
Stephan van Stekelenburg <stephan@hayman.io>
2026-04-09 16:04:23 +0200

    chore: remove uv command, not used

--- setup.sh
+++ setup.sh
@@ -12,9 +12,6 @@ mise install
 echo "Installing dependencies..."
 dotnet restore
 
-# echo "Installing pre-commit hooks..."
-# mise x -- uv run pre-commit install
-
 echo "Copying .env from main clone if it exists..."
 MAIN_CLONE_DIR=$(git rev-parse --path-format=absolute --git-common-dir)/..
 if [ -f "$MAIN_CLONE_DIR/.env" ]; then
